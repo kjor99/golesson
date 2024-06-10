386c7c3cc68b50e891a7706d399d4fb1f9759eef
kjor99 <tyg_1001@163.com>
2024-06-10 19:40:08 +0800

    createusername

--- router/post/login.go
+++ router/post/login.go
@@ -2,6 +2,7 @@ package post
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,19 +13,30 @@ type UserInfo struct {
 	Password string `gorm:"password"`
 }
 
+const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
 func Login(c *gin.Context) {
 
 	var userInfo UserInfo
 
 	c.BindJSON(&userInfo)
-	fmt.Printf("userInfo: %v\n", userInfo)
 
-	fmt.Printf("userInfo.password: %v\n", userInfo.Password)
 	if len(userInfo.Password) < 6 {
 		c.JSON(200, "密码为空或者少于6位数")
 	}
-	if userInfo.Username == "" {
-		c.String(200, "用户名为空", userInfo.Username)
+	if len(userInfo.Username) == 0 {
+		userInfo.Username = randStr(10)
+		c.JSON(200, userInfo.Username)
 	}
+
+}
+
+func randStr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = str[rand.Int63()%int64(len(str))]
+	}
+	fmt.Printf("b: %v\n", b)
+	return string(b)
 
 }
